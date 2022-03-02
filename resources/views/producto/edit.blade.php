6f9dad3e41e47a22112c4662feec069639c0d804
Roberto Cerdan Iturrospe <75723575+RobertoCerdan@users.noreply.github.com>
2022-03-02 18:00:39 +0100

    Create edit.blade.php

---
+++ resources/views/producto/edit.blade.php
@@ -0,0 +1,30 @@
+<div class="container shadow min-vh-100 py-2">
+    <div class="row justify-content-center">
+        <h1 class="text-center mb-5">Plato de albondigas</h1>
+        <div class="col-lg-6">
+            <img class="img-fluid img-thumbnail" src="https://picsum.photos/750/500">
+        </div>
+        <form  class="col-lg-5" action="" method="">
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Email address</label>
+              <input type="number" class="form-control" id="exampleFormControlInput1" min="0">
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+              <label for="formFile" class="form-label">Default file input example</label>
+              <input class="form-control" type="file" id="formFile">
+            </div>
+            <div class="row justify-content-center">
+                <button class="btn btn-primary col-6" type="submit">Editar</button>
+            </div>
+        </form>
+        
+    </div>
+</div>
